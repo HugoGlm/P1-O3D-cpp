2aa5e3620bb7e6feb7292ebbe0004275ec5a1c0f
HugoGlm <hguillaume34@icloud.com>
2022-09-27 11:58:25 +0200

    Maj morpion

--- MyFirstProject/Exo tab 2D/Morpion.cpp
+++ MyFirstProject/Exo tab 2D/Morpion.cpp
@@ -6,11 +6,8 @@ const HANDLE _hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 const int sizeTab = 3, init = 0;
 int shift,
-	cursor,
-	moveZ,
-	moveQ,
-	moveS,
-	moveD;
+isPlayer1,
+cursor[2];
 string Logo = " _ ";
 string Tab[sizeTab][sizeTab] = 
 							   {
@@ -19,37 +16,17 @@ string Tab[sizeTab][sizeTab] =
 								{Logo, Logo, Logo},
 							   };
 
-
-void Move()
-{
-	char _input;
-	int x = 0, y = 1;
-
-
-}
-
-void PlayerOne()
-{
-	SetConsoleTextAttribute(_hConsole, 3); // blue
-	cout << "You can move player 1 (with z, q, s, d) \n";
-	cin >> cursor;
-}
-
-void PlayerTwo()
-{
-	SetConsoleTextAttribute(_hConsole, 4); // red
-	cout << "You can move player 2 (with z, q, s, d) \n";
-	cin >> cursor;
-}
-
 void SetTab()
 {
-	SetConsoleTextAttribute(_hConsole, 2);// green
-
 	for (int i = 0; i < sizeTab; i++)
 	{
 		for (int j = 0; j < sizeTab; j++)
 		{
+			if (cursor[0] == i && cursor[1] == j)
+				SetConsoleTextAttribute(_hConsole, 6);// orange
+			else
+				SetConsoleTextAttribute(_hConsole, 2);// green
+
 			cout << Tab[j][i];
 		}
 		cout << endl;
@@ -57,10 +34,49 @@ void SetTab()
 	SetConsoleTextAttribute(_hConsole, 7); // white
 }
 
+void Move()
+{
+	char _input;
+	cin >> _input;
+	int x = 1, y = 1;
+
+	cursor[0] = x;
+	cursor[1] = y;
+	SetTab();
+
+	if (_input == 'z')
+		x = x == 0 ? 0 : x - 1;
+	else if (_input == 's')
+		x = x == 2 ? 2 : x + 1;
+	else if (_input == 'q')
+		y = y == 0 ? 0 : y - 1;
+	else if (_input == 'd')
+		y = y == 2 ? 2 : y + 1;
+
+	cursor[0] = x;
+	cursor[1] = y;
+
+	system("CLS");
+	SetTab();
+	Move();
+}
+
+void Player()
+{
+	isPlayer1 = !isPlayer1;
+	int _color = (isPlayer1) ? 4 : 3; // 4 = red and 3 = cyan
+	SetConsoleTextAttribute(_hConsole, _color);
+	cout << "You can move (with z, q, s , d)\n";
+}
+
+void SetGame()
+{
+	Player();
+	SetTab();
+	Move();
+}
+
 int main()
 {
-	PlayerOne();
-	SetTab();
-	/*PlayerTwo();
-	SetTab();*/
+	SetGame();
 }
